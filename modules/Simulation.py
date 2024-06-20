ec7afc0616c830317ef0983deed14d9da3d99573
Spiderweak <spiderweak@a-bernard.fr>
2024-06-20 11:04:54 -0400

    doc: documents Simulation.py

--- modules/Simulation.py
+++ modules/Simulation.py
@@ -28,17 +28,29 @@ import json
 TIME_PERIOD = 24 * 60 * 60 * 100
 
 class Simulation(object):
+    """
+    Simulation class to manage the simulation process.
 
+    :param env: The simulation environment.
+    :type env: Environment
+    """
     def __init__(self, env):
+        """
+        Initializes the Simulation instance.
 
+        :param env: The simulation environment.
+        :type env: Environment
+        """
         self.__env: Environment = env
         self.__queue = EventQueue(self.__env)
 
         # Deploying X applictions
         #arrival_times = [int(time) for time in np.random.uniform(0, TIME_PERIOD, env.config.number_of_applications)]
 
-
     def init_simulation(self):
+        """
+        Initializes the simulation by generating arrival times and events.
+        """
         if self.__env.config is None:
             raise ValueError
 
@@ -58,19 +70,15 @@ class Simulation(object):
             device_id = random.choice(range(len(self.__env.devices)))
 
             # Creating a placement event
-            Placement("Placement",self.__queue, application, device_id, event_time=arrival_times[i]).add_to_queue()
-
-
+            Placement("Placement", self.__queue, application, device_id, event_time=arrival_times[i]).add_to_queue()
         # Final reporting event
 
         FinalReport("Final Report", self.__queue, event_time=TIME_PERIOD).add_to_queue()
 
-        # Init devices and network here ?
-
-
     def simulate(self):
-        # main loop of the simulation
-
+        """
+        Main loop of the simulation.
+        """
         current_event = None
 
         print("\nRunning The Event Queue")
@@ -87,15 +95,16 @@ class Simulation(object):
             self.__env.current_time = event_time
 
             process_event = current_event.process(self.__env)
-            logging.debug("process_event: {}".format(process_event))
+            logging.debug(f"process_event: {process_event}")
 
             previous_time = event_time
 
-        logging.info("\n***************\nEND OF SIMULATION\n***************")
+        logging.info("\n**********\nEND OF SIMULATION\n**********")
 
     def full_state_simulate(self):
-        # alt loop of the simulation
-
+        """
+        Alternate loop of the simulation.
+        """
         current_event = None
 
         print("\nRunning The Event Queue")
@@ -118,17 +127,17 @@ class Simulation(object):
                 self.__env.extract_currently_deployed_apps_data()
 
             process_event = current_event.process(self.__env)
-            logging.debug("process_event: {}".format(process_event))
+            logging.debug(f"process_event: {process_event}")
 
             previous_time = event_time
 
-        time.sleep(1)
-
-        logging.info("\n***************\nEND OF SIMULATION\n***************")
-
+        logging.info("\n**********\nEND OF SIMULATION\n**********")
 
 
     def import_queue_items(self):
+        """
+        Imports queue items from a JSON file.
+        """
         if self.__env.config is None:
             raise ValueError
 
@@ -142,11 +151,14 @@ class Simulation(object):
             application = self.__env.get_application_by_id(item["application"])
             device_id = item["requesting_device"]
             arrival_time = item["placement_time"]
-            Placement("Placement",self.__queue, application, device_id, event_time=arrival_time).add_to_queue()
+            Placement("Placement", self.__queue, application, device_id, event_time=arrival_time).add_to_queue()
 
         FinalReport("Final Report", self.__queue, event_time=TIME_PERIOD).add_to_queue()
 
     def bulk_import_queue_items(self):
+        """
+        Bulk imports queue items from a JSON file.
+        """
         try:
             with open(self.__env.config.arrivals_file) as file:
                 arrivals_list = json.load(file)
