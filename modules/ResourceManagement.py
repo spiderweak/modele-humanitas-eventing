158bd6b167f89c94e8cd7baf7416e5ea2617d075
Spiderweak <spiderweak@a-bernard.fr>
2024-06-20 10:16:36 -0400

    RST-compatible ResourceManagement

--- modules/ResourceManagement.py
+++ modules/ResourceManagement.py
@@ -1,27 +1,45 @@
+"""
+ResourceManagement Module
+
+This module provides utility functions for managing resources in a simulated environment.
+It includes functions for calculating the resource fit and custom distance for devices.
+
+Functions:
+    fit_resource(new_value, limit_value): Calculates the fit resource value.
+    custom_distance(A, B): Calculates a custom distance metric between two sets of coordinates.
+
+Usage Example:
+    fit = fit_resource(new_value, limit_value)
+    distance = custom_distance(A, B)
+"""
 
 def fit_resource(new_value, limit_value):
+    """
+    Calculates the fit resource value.
+
+    :param new_value: The new value to fit.
+    :type new_value: float
+    :param limit_value: The limiting value.
+    :type limit_value: float
+    :return: The fit resource value.
+    :rtype: float
+    """
     try:
         return min(limit_value / new_value, 1)
     except ZeroDivisionError:
         return 1
 
-
-def custom_distance(A,B):
+def custom_distance(A, B):
     """
     Defines a custom distance for device wireless coverage to account for less coverage due to floor interception.
 
-    If A and B are not of the same size, distance is calculated among the first values up to min(len(A),len(B))
+    If A and B are not of the same size, distance is calculated among the first values up to min(len(A), len(B)).
 
-    Args:
-    -----
-    A : `list`
-        List of coords for point A
-    B : `list`
-        List of coords for point B
-
-    Returns:
-    --------
-    distance : `int`
-        Distance (as coverage) between A and B.
+    :param A: List of coordinates for point A.
+    :type A: list
+    :param B: List of coordinates for point B.
+    :type B: list
+    :return: Distance (as coverage) between A and B.
+    :rtype: float
     """
-    return sum([((b-a)**2) for a,b in zip(A,B)]) ** 0.5
+    return sum([(b - a) ** 2 for a, b in zip(A, B)]) ** 0.5
